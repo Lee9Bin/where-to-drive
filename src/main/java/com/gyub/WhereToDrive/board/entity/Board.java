8922c44a4cde1b0df32a9790bf046a4d983d902e
Lee9Bin <imagen33@daum.net>
2024-05-12 01:21:33 +0900

    feat : @ToString 추가

--- src/main/java/com/gyub/WhereToDrive/board/entity/Board.java
+++ src/main/java/com/gyub/WhereToDrive/board/entity/Board.java
@@ -3,10 +3,12 @@ package com.gyub.WhereToDrive.board.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Board {
 
     public int bid;
