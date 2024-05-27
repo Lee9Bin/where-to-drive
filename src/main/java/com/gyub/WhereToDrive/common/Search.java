747a4d47383e9d8a0d959bcc3415dfe99dafa47e
Lee9Bin <imagen33@daum.net>
2024-05-27 23:30:56 +0900

    feat : lombok 추가

--- src/main/java/com/gyub/WhereToDrive/common/Search.java
+++ src/main/java/com/gyub/WhereToDrive/common/Search.java
@@ -1,41 +1,15 @@
 package com.gyub.WhereToDrive.common;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Search extends Pagination{
 
 	private String searchType;
-
-	private String keyword;	
-
-			
-
-	public String getSearchType() {
-
-		return searchType;
-
-	}
-
-	
-
-	public void setSearchType(String searchType) {
-
-		this.searchType = searchType;
-
-	}
-
-	
-
-	public String getKeyword() {
-
-		return keyword;
-
-	}
-
-
-
-	public void setKeyword(String keyword) {
-
-		this.keyword = keyword;
-
-	}
+	private String keyword;
 
 }
